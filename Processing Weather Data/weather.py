3b0c792093bb0e5da34e906d3fd192ff2851904c
Khalid Hussain <53101332+khalidh223@users.noreply.github.com>
2019-12-12 16:24:10 -0600

    Edits to starter code

--- Processing Weather Data/weather.py
+++ Processing Weather Data/weather.py
@@ -1,6 +1,7 @@
 '''
     weather.py
     Jeff Ondich, 2 October 2018
+    modified by Khalid Hussain, 7 October 2018
 
     Starter code for the weather data assignment in CS 111.
 
@@ -28,12 +29,12 @@
 import sys
 import csv
 
-def get_weather_data(csv_file_name):
+def get_weather_data(weather):
     ''' Returns a list of lists representing the lines of the
         specified comma-separated values (CSV) file. See notes
         above for further information. '''
     weather_data = []
-    with open(csv_file_name) as csv_file:
+    with open(weather) as csv_file:
         reader = csv.reader(csv_file)
         header = next(reader)
         header_length = len(header)
@@ -50,7 +51,7 @@ def get_highest_temperature(weather_data):
         along with the year in which it was recorded. If the highest temperature
         occurred more than once, the year returned is the most recent year in
         which the highest temperature occurred.
-        
+
         NOTE THAT THIS FUNCTION IS UNFINISHED. You'll have to handle the year
         yourself.
     '''
@@ -62,12 +63,101 @@ def get_highest_temperature(weather_data):
             max_temperature = float(max_temperature_string)
             if max_temperature > max_temperature_ever:
                 max_temperature_ever = max_temperature
+                for max_temperature in row:
+                    year_string = row[0]
+                    year_of_temp = int(year_string)
+                    if year_of_temp > year:
+                        year = year_of_temp
     return max_temperature_ever, year
+
+def get_lowest_temperature(weather_data):
+    min_temperature_ever = 200.0
+    year = 1872
+    '''
+    I set year here to 1872 because my program kept outputting 1888 for the
+    minimum temperature, instead of the correct year of 1872. What Python would
+    do is it would output the correct temperature (-41), but would also output
+    the year where this temperature was positive instead of negative (1888).  
+    '''
+    for row in weather_data:
+        min_temperature_string = row[4]
+        if min_temperature_string != 'M':
+            min_temperature = float(min_temperature_string)
+            if min_temperature < min_temperature_ever:
+                min_temperature_ever = min_temperature
+                for min_temperature in row:
+                    year_string = row[0]
+                    year_of_temp = int(year_string)
+                    if year_of_temp < year:
+                        year = year_of_temp
+    return min_temperature_ever, year
+
+def avg_high_temperature(weather_data):
+    total = 0.0
+    count = 0
+    for row in weather_data:
+        high_temperature_strings = row[3]
+        if high_temperature_strings != "M":
+            actual_temps = float(high_temperature_strings)
+            total = total + actual_temps
+            count = count + 1
+    output = print("The average high temperature is: {0:0.2f} F".format(total/count))
+    return output
+
+def avg_low_temperature(weather_data):
+    total = 0.0
+    count = 0
+    for row in weather_data:
+        low_temperature_strings = row[4]
+        if low_temperature_strings != "M":
+            actual_temps = float(low_temperature_strings)
+            total = total + actual_temps
+            count = count + 1
+    output = print("The average low temperature is: {0:0.2f} F".format(total/count))
+    return output
+
+def highest_snowfall_day(weather_data):
+    max_snowfall_ever = -200.0
+    for row in weather_data:
+        max_snowfall_string = row[6]
+        max_snowfall_year = row[0]     # This points Python to the year column, which is really position 0 of all the rows in the file
+        max_snowfall_month = row[1]
+        max_snowfall_day = row[2]
+        if max_snowfall_string != 'M' and max_snowfall_string != 'T':
+            max_snowfall = float(max_snowfall_string)
+            if max_snowfall > max_snowfall_ever: # This points to the specific line of the maximum snowfall
+                max_snowfall_ever = max_snowfall
+                year = max_snowfall_year     # This tells Python to look at the year of this line, located in row[0] (which was assigned earlier to max_snowfall_year)
+                month = max_snowfall_month
+                day = max_snowfall_day
+    return max_snowfall_ever, year, month, day
+
+def highest_precipitation_day(weather_data):
+    max_precipitation_ever = -200.0
+    for row in weather_data:
+        max_precipitation_string = row[5]
+        max_precipitation_year = row[0]
+        max_precipitation_month = row[1]
+        max_precipitation_day = row[2]
+        if max_precipitation_string != 'M' and max_precipitation_string != 'T':
+            max_precipitation = float(max_precipitation_string)
+            if max_precipitation > max_precipitation_ever:
+                max_precipitation_ever = max_precipitation
+                year = max_precipitation_year
+                month = max_precipitation_month
+                day = max_precipitation_day
+    return max_precipitation_ever, year, month, day
 
 def main():
     weather_data = get_weather_data('weather.csv')
     highest_temperature, year = get_highest_temperature(weather_data)
-    print('The highest temperature ever: {0:.2f} ({1})'.format(highest_temperature, year))
-
+    print('The highest temperature ever is: {0:.2f} F ({1})'.format(highest_temperature, year))
+    min_temperature, year = get_lowest_temperature(weather_data)
+    print('The lowest temperature ever is: {0:.2f} F ({1})'.format(min_temperature, year))
+    avg_high_temp = avg_high_temperature(weather_data)
+    avg_low_temp = avg_low_temperature(weather_data)
+    max_snowfall, year, month, day = highest_snowfall_day(weather_data)
+    print('Highest snowfall in a day: ', max_snowfall, 'inches', '({0:>0}-{1:>0}-{2:>0})'.format(year, month, day))
+    max_precipitation, year, month, day = highest_precipitation_day(weather_data)
+    print('Highest precipitation in a day: ', max_precipitation, 'inches', '({0:>0}-{1:>0}-{2:>0})'.format(year, month, day))
 main()
-
